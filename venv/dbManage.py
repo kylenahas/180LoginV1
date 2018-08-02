06561cf06801c8f55b4c60350212400678419d37
Kyle <kylenahas@gmail.com>
2018-08-01 17:32:30 -0700

    Now member IDs are guaranteed to be unique. Also fixed a edge case where IDs could be less than 16 characters.

--- venv/dbManage.py
+++ venv/dbManage.py
@@ -12,8 +12,10 @@ import pprint
 import hashlib
 
 from datetime import datetime, date, time, timedelta
-from tinydb import TinyDB, Query
+import time
+from tinydb import *
 from tinydb.operations import decrement
+import re
 
 
 class LoginDatabase:
@@ -25,21 +27,27 @@ class LoginDatabase:
         exp_date = "-1"
         exp_punches = 0
 
+        member_ID_str = first_name + last_name + str(join_date)
+        member_ID = int(hashlib.sha256(member_ID_str.encode('utf-8')).hexdigest(),
+                        16) % 10 ** 16  # Generate a 16 digit ID number :: https://stackoverflow.com/a/42089311
+
+        while self.membersDB.contains(Query().id == member_ID) or len(str(member_ID)) < 16:    # Check for member_id uniqueness
+            time.sleep(0.1)
+            join_date = datetime.now()
+            member_ID_str = first_name + last_name + str(join_date)
+            member_ID = int(hashlib.sha256(member_ID_str.encode('utf-8')).hexdigest(),
+                            16) % 10 ** 16  # Generate a 16 digit ID number :: https://stackoverflow.com/a/42089311
+
         if member_type_str == "monthly":
             exp_date = str(timedelta(days=30) + join_date)
         elif member_type_str == "annual":
             exp_date = str(timedelta(years=1) + join_date)
         elif member_type_str == "student":
             exp_date = str(timedelta(days=30) + join_date)
-        else:
+        elif member_type_str == "punchcard":
             exp_punches = 10
 
-        member_ID_str = first_name + last_name  # TODO: Add join datetime to member ID pre hash
-        member_ID = int(hashlib.sha256(member_ID_str.encode('utf-8')).hexdigest(),
-                        16) % 10 ** 16  # Generate a 16 digit ID number :: https://stackoverflow.com/a/42089311
 
-
-        # TODO: Check uniqueness of new ID, wait 1 second, update join_date and retry. Repeat until a unique ID is assigned.
 
         entry = {"name_first": first_name,
                  "name_last": last_name,
@@ -128,88 +136,13 @@ class LoginDatabase:
         else:
             raise ValueError("The entered user ID could not be found in the database")
 
+    def query_member(self, name_first):
+        member = Query()
+        results = self.membersDB.search(member.name_first.matches(re.compile(name_first, re.IGNORECASE)))
 
+        if not results:
+            raise ValueError("The entered last name could not be found in the database!")
 
-# join_date = datetime.now()
-# exp_date = timedelta(days=30)
-# exp_date += join_date
-#
-# member_ID_str = first_name + last_name      # TODO: Add join datetime to member ID pre hash
-# member_ID = int(hashlib.sha256(member_ID_str.encode('utf-8')).hexdigest(), 16) % 10 ** 16  # Generate a 16 digit ID number :: https://stackoverflow.com/a/42089311
-#
-
-
-
-
-
-#
-# entry = {"name_first": first_name, "name_last": last_name, "id": member_ID, "dob": str(birthdate), "join_date": str(join_date),
-#          "member_type": member_type_str, "expiration_date": str(exp_date), "expiration_punches": 10}
-
-# entry["expiration_punches"] -= 1
-
-
-# print(entry)
-
-# membersDB.insert(entry)
-
-# print(sys.version_info)
-
-# print(membersDB.all())
-
-# ld = LoginDatabase()
-
-
-first_name_a = "Jessica"
-last_name_a = "Johnson"
-
-email_a = "email@example.com"
-
-phone_a = "(707) 123-4567"
-
-birthdate_a = date(1994, 6, 5)
-
-member_type_str_a = "punchcard"
-
-
-# ld.add_member(first_name_a, last_name_a, email_a, phone_a, birthdate_a, member_type_str_a)
-
-
-# pp = pprint.PrettyPrinter(indent=4)
-#
-# pp.pprint(ld.membersDB.all())
-
-# print()
-# print("=====================")
-# print("=========LOG:========")
-# print("=====================")
-
-
-
-# current_M_ID = ld.membersDB.member_ID
-
-# ld.log_member(LoginDatabase, current_M_ID)
-
-# logged_time = datetime.now()
-#
-# member_query = Query()
-# member_data = membersDB.get(member_query.id == current_M_ID)
-# # print(member_data["name_last"] + ", " + member_data["name_first"])
-#
-# membersDB.update(decrement('expiration_punches'), member_query.id == current_M_ID)
-#
-# log_entry = {"id": current_M_ID, "name_first": member_data["name_first"], "name_last": member_data["name_last"],
-#              "log_time": str(logged_time), "remaining_punches": member_data["expiration_punches"]}
-#
-# logDB.insert(log_entry)
-
-
-
-# pp.pprint(ld.log_member(3229308233395595))
-#
-# pp.pprint(ld.logDB.all())
-
-
-# print(logDB.count(member_query.id == current_M_ID))
+        return results
 
 
